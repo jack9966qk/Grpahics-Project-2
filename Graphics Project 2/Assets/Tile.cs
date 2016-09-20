049fc0ac995182d3df028796ee00b4750498a9dd
Weixu Chen <kevinchen@Kevins-MacBook-Pro-2.local>
2016-09-20 15:27:05 +1000

    Added an algorithm for tile generation

---
+++ Graphics Project 2/Assets/Tile.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Tile : MonoBehaviour {
+
+
+	public Shader shader;
+
+	// Update is called once per frame
+	void Update () {
+
+
+
+	}
+
+	public void CreateTileMesh(Vector3 frontOrigin, Vector3 backOrigin,float degree,float radius){
+		
+		Mesh m = new Mesh();
+		m.name = "Tile";
+
+
+		float radian = degree * Mathf.PI / 180;
+		float radian30 = 30 * Mathf.PI / 180;
+		// See figure on Widipedia Dodecagon figure
+
+		List<Vector3> vertices = new List<Vector3>();
+		List<Color> colors = new List<Color>();
+		Color r = new Color(Random.value,Random.value,Random.value,1f);
+
+		vertices.Add(new Vector3(Mathf.Sin(radian)*radius+frontOrigin.x,Mathf.Cos(radian)*radius+frontOrigin.y,frontOrigin.z));
+		vertices.Add(new Vector3(Mathf.Sin(radian+radian30)*radius+frontOrigin.x, Mathf.Cos(radian+radian30)*radius+frontOrigin.y, frontOrigin.z));
+		vertices.Add(new Vector3(Mathf.Sin(radian+radian30)*radius+backOrigin.x, Mathf.Cos(radian+radian30)*radius+backOrigin.y, backOrigin.z));
+		vertices.Add(new Vector3(Mathf.Sin(radian)*radius+frontOrigin.x,Mathf.Cos(radian)*radius+frontOrigin.y,frontOrigin.z));
+		vertices.Add(new Vector3(Mathf.Sin(radian+radian30)*radius+backOrigin.x, Mathf.Cos(radian+radian30)*radius+backOrigin.y, backOrigin.z));
+		vertices.Add(new Vector3(Mathf.Sin(radian)*radius+backOrigin.x,Mathf.Cos(radian)*radius+backOrigin.y,backOrigin.z));
+
+
+		colors.Add(r);
+		colors.Add(r);
+		colors.Add(r);
+		colors.Add(r);
+		colors.Add(r);
+		colors.Add(r);
+
+
+		m.SetVertices (vertices);
+		m.SetColors (colors);
+		//m.SetNormals(this.normals);
+		// Automatically define the triangles based on the number of vertices
+		int[] triangles = new int[m.vertices.Length];
+		for (int i = 0; i < m.vertices.Length; i++)
+			triangles[i] = i;
+
+		m.triangles = triangles;
+
+
+
+		MeshFilter tileMesh = this.gameObject.AddComponent<MeshFilter>();
+		tileMesh.mesh = m;
+
+		// Add a MeshRenderer component. This component actually renders the mesh that
+		// is defined by the MeshFilter component.
+		MeshRenderer renderer = this.gameObject.AddComponent<MeshRenderer>();
+		renderer.material.shader = shader;
+	}
+
+
+	private void Destroy(){
+
+
+	}
+
+}
--- Graphics Project 2/Assets/Tunnel.cs
+++ Graphics Project 2/Assets/Tunnel.cs
@@ -4,28 +4,14 @@ using System.Collections.Generic;
 public class Tunnel : MonoBehaviour {
 
 
-	public Shader shader;
+	public GameObject tile;
 
 	private const float RADIUS = 1f;
 
 
-	private List<Vector3> vertices = new List<Vector3>();
-	private List<Color> colors = new List<Color>();
-	private List<Vector3> normals = new List<Vector3>();
-
 	// Use this for initialization
 	void Start () {
-		
-		MeshFilter tunnelMesh = this.gameObject.AddComponent<MeshFilter>();
-		tunnelMesh.mesh = this.CreateTunnelMesh();
-
-		// Add a MeshRenderer component. This component actually renders the mesh that
-		// is defined by the MeshFilter component.
-		MeshRenderer renderer = this.gameObject.AddComponent<MeshRenderer>();
-
-
-		renderer.material.shader = shader;
-
+		GenerateTunnel ();
 	}
 	
 	// Update is called once per frame
@@ -34,65 +20,28 @@ public class Tunnel : MonoBehaviour {
 	}
 
 
-	private Mesh CreateTunnelMesh(){
-		Mesh m = new Mesh();
-		m.name = "Tunnel";
-
-		Vector3 frontOrigin;
-		Vector3 backOrigin;
+	private void GenerateTunnel(){
+		
+		Vector3 frontOrigin = new Vector3 (0, 0, 0);
+		Vector3 backOrigin = new Vector3 (0, 0, 0);
 
 		for (int i = 0; i < 20; i++) {
-			frontOrigin = new Vector3 (0, 0, i);
-			backOrigin = new Vector3 (0, 0, i + 1);
+
+			//backOrigin = new Vector3(0f,Mathf.Sin(i*Mathf.PI/10),i);
+			backOrigin = new Vector3(0f,0f,i);
+
 			for(float degree = 0; degree < 360;degree = degree + 30){
-				GenerateSideSquare (frontOrigin,backOrigin,degree);
+				GameObject t = Instantiate (tile);
+				t.GetComponent<Tile>().CreateTileMesh(frontOrigin,backOrigin,degree,RADIUS);
+
 			}
 
-		}
-
-		m.SetVertices(this.vertices);
-		m.SetColors(this.colors);
-		//m.SetNormals(this.normals);
-		// Automatically define the triangles based on the number of vertices
-		int[] triangles = new int[m.vertices.Length];
-		for (int i = 0; i < m.vertices.Length; i++)
-			triangles[i] = i;
-
-		m.triangles = triangles;
-
-		return m;
-	}
-
-
-	public void GenerateSideSquare(Vector3 frontOrigin, Vector3 backOrigin,float degree){
-
-		float radian = degree * Mathf.PI / 180;
-		float radian30 = 30 * Mathf.PI / 180;
-		// See figure on Widipedia Dodecagon figure
-		vertices.Add(new Vector3(Mathf.Sin(radian)*RADIUS+frontOrigin.x,Mathf.Cos(radian)*RADIUS+frontOrigin.y,frontOrigin.z));
-		vertices.Add(new Vector3(Mathf.Sin(radian+radian30)*RADIUS+frontOrigin.x, Mathf.Cos(radian+radian30)*RADIUS+frontOrigin.y, frontOrigin.z));
-		vertices.Add(new Vector3(Mathf.Sin(radian+radian30)*RADIUS+backOrigin.x, Mathf.Cos(radian+radian30)*RADIUS+backOrigin.y, backOrigin.z));
-		vertices.Add(new Vector3(Mathf.Sin(radian)*RADIUS+frontOrigin.x,Mathf.Cos(radian)*RADIUS+frontOrigin.y,frontOrigin.z));
-		vertices.Add(new Vector3(Mathf.Sin(radian+radian30)*RADIUS+backOrigin.x, Mathf.Cos(radian+radian30)*RADIUS+backOrigin.y, backOrigin.z));
-		vertices.Add(new Vector3(Mathf.Sin(radian)*RADIUS+backOrigin.x,Mathf.Cos(radian)*RADIUS+backOrigin.y,backOrigin.z));
-
-		Color c = new Color (0, (Mathf.Sin (radian) + 1) / 2, 0, 1);
-		if (Mathf.Round(frontOrigin.z)% 2 == 1) {
-			c = new Color (0, (Mathf.Sin (radian) + 1) / 2, 0, 1);
-		} else {
-			c = new Color (0, (Mathf.Cos (radian) + 1) / 2, 0, 1);
+			frontOrigin = backOrigin;
 
 		}
 
-		colors.Add(c);
-		colors.Add(c);
-		colors.Add(c);
-		colors.Add(c);
-		colors.Add(c);
-		colors.Add(c);
-
-
 	}
+
 
 
 }
