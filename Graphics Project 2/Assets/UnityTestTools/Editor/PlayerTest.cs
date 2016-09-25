2335182c394c5812189bd6ef022ef0d45e132a3d
jack9966qk <jack9966qk@gmail.com>
2016-09-25 22:32:34 +1000

    Setting up unit test tools

---
+++ Graphics Project 2/Assets/UnityTestTools/Editor/PlayerTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class PlayerTest {
+
+    private Player p = new Player(100);
+
+    [SetUp]
+    public void setup() {
+
+    }
+
+	[Test]
+	public void EditorTest()
+	{
+		//Arrange
+		var gameObject = new GameObject();
+
+		//Act
+		//Try to rename the GameObject
+		var newGameObjectName = "My game object";
+		gameObject.name = newGameObjectName;
+
+		//Assert
+		//The object has a new name
+		Assert.AreEqual(newGameObjectName, gameObject.name);
+	}
+}
