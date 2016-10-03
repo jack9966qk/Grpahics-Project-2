f718976a8774e84eabfd825e25de982c85da4573
jack9966qk <jack9966qk@gmail.com>
2016-10-03 19:26:46 +1100

    Added third person camera and camera control
    - Added CameraController to handle camera control, support switching main camera in runtime
    - Added settings to change camera view, along with UI in settings page

--- Graphics Project 2/Assets/GameSettings.cs
+++ Graphics Project 2/Assets/GameSettings.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 
 public class GameSettings  {
+    public enum CameraSetting { FirstPerson, ThirdPerson }
     public float acclerometerSensitivity = 40f;
     public Vector3 acclerometerOffset = Vector3.zero;
+    public CameraSetting cameraSetting = CameraSetting.FirstPerson;
 }
