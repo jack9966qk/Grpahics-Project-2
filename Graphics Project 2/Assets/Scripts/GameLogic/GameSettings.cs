631caa1c40ce7cf070f458d01a55a41d35cf2a1c
jack9966qk <jack9966qk@gmail.com>
2016-10-17 20:03:46 +1100

    - Changed player model (spaceship) - Added particle system for afterblow of spaceship

--- Graphics Project 2/Assets/Scripts/GameLogic/GameSettings.cs
+++ Graphics Project 2/Assets/Scripts/GameLogic/GameSettings.cs
@@ -4,5 +4,5 @@ public class GameSettings  {
     public enum CameraSetting { FirstPerson, ThirdPerson }
     public float acclerometerSensitivity = 40f;
     public Vector3 acclerometerOffset = Vector3.zero;
-    public CameraSetting cameraSetting = CameraSetting.FirstPerson;
+    public CameraSetting cameraSetting = CameraSetting.ThirdPerson;
 }
