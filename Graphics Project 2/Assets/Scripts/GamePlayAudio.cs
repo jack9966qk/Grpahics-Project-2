e85bfaaa739066e4b197242d612660e17ee9f94a
jack9966qk <jack9966qk@gmail.com>
2016-10-25 14:43:25 +1100

    Added simple background music
    - Gameplay BGM as `Game.wav`
    - Menu, settings and instructions BGM as `Menu.wav`
    - Scripts for the music, make `Menu.wav` play accros multiple scenes

---
+++ Graphics Project 2/Assets/Scripts/GamePlayAudio.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamePlayAudio : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    foreach (var obj in GameObject.FindGameObjectsWithTag("MainMenuMusic")) {
+            Destroy(obj);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Graphics Project 2/Assets/Scripts/MainMenuAudio.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuAudio : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (GameObject.FindGameObjectsWithTag("MainMenuMusic").Length > 1) {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+	}
+}
